ab022b31eb85a167c6f8257a3571e7af45433e3b
LittleSocksPetHotel <chris@littlesockspethotel.com>
2026-07-13 19:41:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 
 // Step 1: Create your real Google Form.
 // Step 2: Replace the placeholder link below with your real Google Form URL.
-const BOOKING_FORM_URL = "https:https://docs.google.com/forms/d/e/1FAIpQLSftZxTAw-UsZBtrj0l02xMFpnldY8SociJpV_ycvSw5lvjqvQ/viewform";
+const BOOKING_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSftZxTAw-UsZBtrj0l02xMFpnldY8SociJpV_ycvSw5lvjqvQ/viewform";
 
 // This updates every button/link with class="booking-link" to use the same form link.
 document.querySelectorAll(".booking-link").forEach((link) => {
